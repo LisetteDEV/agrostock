42c353613be9ddd1bbe7d36a79b6d08ca0dad62a
Lisette <obognonlisette@gmail.com>
2026-07-18 17:48:18 +0100

    fix(cors): make cors completely permissive for all paths and origins

--- agrostock-backend/config/cors.php
+++ agrostock-backend/config/cors.php
@@ -15,15 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-'allowed_origins' => [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://agrostockbenin.vercel.app',
-],
+'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -32,6 +28,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
